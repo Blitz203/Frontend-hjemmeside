aaec6a93ee5c340e25d76a32f4ccaff77f60ca1c
Mikkel <mikk284p@edu.sde.dk>
2024-11-26 12:47:33 +0100

    feat(menu) categorized alle menuItems on one page instead of multiple

--- app/AllMenuItems/page.tsx
+++ app/AllMenuItems/page.tsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from "react";
 import Sidebar from "../components/sideBar";
-import { Menu } from "lucide-react";
 import Navbar from "../components/navBar";
 
 const MenuItems = () => {
@@ -13,7 +12,7 @@ const MenuItems = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("http://localhost:5005/menu_items", {
+        const response = await fetch("http://localhost:5005/Menu", {
           method: "GET",
         });
 
@@ -24,7 +23,7 @@ const MenuItems = () => {
         const result = await response.json();
         console.log(result);
 
-        setData(result);
+        setData(result); // Set the fetched data
       } catch (err: any) {
         setError(err.message);
       } finally {
@@ -47,17 +46,24 @@ const MenuItems = () => {
     <div className="h-screen flex flex-col">
       <Navbar />
       <div className="flex-1 p-8 bg-gray-100 overflow-auto">
-        <div className="flex flex-wrap gap-6">
-          {data.map((item) => (
-            <div
-              key={item.id}
-              className="box-border h-64 w-64 p-4 border-4 flex flex-col justify-between bg-white shadow-md"
-            >
-              <h3 className="text-lg font-semibold">{item.name}</h3>
-              <p className="text-sm">{item.description}</p>
-              <p className="text-lg font-bold">
-                {(item.price_in_oere / 100).toFixed(2)} kr
-              </p>
+        <div className="flex flex-col gap-6">
+          {data.map((category) => (
+            <div key={category.name} className="mb-8">
+              <h2 className="text-2xl font-semibold mb-4">{category.name}</h2>
+              <div className="flex flex-wrap gap-6">
+                {category.menu_item.map((item: any) => (
+                  <div
+                    key={item.name}
+                    className="box-border h-64 w-64 p-4 border-4 flex flex-col justify-between bg-white shadow-md cursor-pointer hover:opacity-40"
+                  >
+                    <h3 className="text-lg font-semibold">{item.name}</h3>
+                    <p className="text-sm">{item.description}</p>
+                    <p className="text-lg font-bold">
+                      {(item.price_in_oere / 100).toFixed(2)} kr
+                    </p>
+                  </div>
+                ))}
+              </div>
             </div>
           ))}
         </div>
